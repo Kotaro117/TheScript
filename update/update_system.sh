e4d4beade9ef14daa148a7f7d56226af146ab429
Kotaro117 <hobble-choir-0m@icloud.com>
2024-02-29 10:41:32 +0000

    Update update_system.sh
    added time stamps after the echo commands

--- update/update_system.sh
+++ update/update_system.sh
@@ -7,19 +7,20 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # No Colour
 
 
-VERSION="2.7.5"
+VERSION="2.7.6"
 RELEASE_FILE="/etc/os-release"
+TIME_STAMP=$(date +"%d/%m/%Y %H:%M:%S")
 
 function exit_code() {
     if [ $? -eq 0 ]
     then
-        echo -e "${GREEN}Update was successful ${NC}"
+        echo -e "${GREEN}Update was successful $TIME_STAMP ${NC}"
     else
-        echo -e "${RED}Update was not successful ${NC}"
+        echo -e "${RED}Update was not successful $TIME_STAMP ${NC}"
     fi
 }
 
-echo -e "${YELLOW}running Version $VERSION ${NC}"
+echo -e "${YELLOW}running Version $VERSION $TIME_STAMP ${NC}"
 
 if grep -q "Arch" $RELEASE_FILE
 then
@@ -27,7 +28,7 @@ then
     sudo pacman -Syu
     exit_code                                                               # Checks if the update was successful
 elif
-    echo -e "${YELLOW}Host is not based on Arch ${NC}"
+    echo -e "${YELLOW}Host is not based on Arch $TIME_STAMP ${NC}"
     grep -q "Debian" $RELEASE_FILE || grep -q "Ubuntu" $RELEASE_FILE || grep -q "Pop" $RELEASE_FILE # "||"=or
 then
     # The host is based on Debian, Ubuntu or Pop
@@ -35,5 +36,5 @@ then
     sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
     exit_code                                                               # Checks if the update was successful
 else
-    echo -e "${YELLOW}Host is not based on Debian ${NC}"
+    echo -e "${YELLOW}Host is not based on Debian $TIME_STAMP ${NC}"
 fi
