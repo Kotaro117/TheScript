eb2c3b8fe41b89698eb8d35ca42a16066e417528
Kotaro117 <hobble-choir-0m@icloud.com>
2024-02-29 08:19:39 +0000

    Update update_system.sh
    Added a function for troubleshooting

--- update/update_system.sh
+++ update/update_system.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-VERSION="2.5"
+VERSION="2.6"
 RELEASE_FILE="/etc/os-release"
 
 function exit_code() {
-
     if [ $? -eq 0 ]
     then
-        exit
+        echo "Update was successful"
+    else
+        echo "Update was not successful"
     fi
 }
 
@@ -16,12 +17,18 @@ echo "running Version $VERSION"
 if grep -q "Arch" $RELEASE_FILE
 then
     # The host is based on Arch, run the pacman update command
-    sudo pacman -Syu 
+    sudo pacman -Syu
+    exit_code
+else
+    echo "Host is not based on Arch"
 fi
 
 if grep -q "Debian" $RELEASE_FILE || grep -q "Ubuntu" $RELEASE_FILE || grep -q "Pop" $RELEASE_FILE # "||"=or
 then
-    # The host is based on Ubuntu or Debian,
+    # The host is based on Debian, Ubuntu or Pop
     # Run the apt version of the command
     sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
+    exit_code
+else
+    echo "Host is not based on Debian"
 fi
