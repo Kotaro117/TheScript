9a460e7d313ebdac3bbd06103742ba6e14e3d055
Kotaro117 <hobble-choir-0m@icloud.com>
2024-02-29 13:55:16 +0000

    Update install_proxmoxGuestAgent.sh
    added some lines for easier troubleshooting

--- install/install_proxmoxGuestAgent.sh
+++ install/install_proxmoxGuestAgent.sh
@@ -1,12 +1,43 @@
 #!/bin/bash
 
-# V 0.9
+VERSION="0.10"
+PACKAGE_NAME="qemu-guest-agent"
+TIME_STAMP=$(date +"%d/%m/%Y %H:%M:%S")
+# Define colour codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Colour
+
+
+function exit_code() {
+    if [ $? -eq 0 ]
+    then
+        echo -e "${GREEN}$COMMAND was successful $TIME_STAMP ${NC}"
+    else
+        echo -e "${RED}$COMMAND was not successful $TIME_STAMP ${NC}"
+    fi
+}
+
+echo -e "${YELLOW}running Version $VERSION of the script $TIME_STAMP ${NC}"
 
 # Install Proxmox guest agent
-sudo apt-get install qemu-guest-agent -y
+COMMAND="Installatiom of the Proxmox guest agent"
+if  dpkg -s "$PACKAGE_NAME" | grep "Status: install ok installed"
+then
+    echo -e "${YELLOW}$PACKAGE_NAME is allready installed $TIME_STAMP ${NC}"
+else
+    sudo apt-get install -y $PACKAGE_NAME
+    exit_code
+fi
+
 
 # Start the agent
-sudo systemctl start qemu-guest-agent
+COMMAND="Starting of the agent"
+sudo systemctl start $PACKAGE_NAME
+exit_code
 
 # enable the service to autostart (permanently) if not auto started
-sudo systemctl enable qemu-guest-agent
+COMMAND="Enabling of the service"
+sudo systemctl enable $PACKAGE_NAME
+exit_code
