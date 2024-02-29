7a47ab1f6dc263807104acaa837bedee05bf0f60
Kotaro117 <hobble-choir-0m@icloud.com>
2024-02-29 12:11:44 +0000

    Update update_system.sh
    replaced the variable declaration. Added a message, that outputs if the host is based on Arch or Debian

--- update/update_system.sh
+++ update/update_system.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
+VERSION="2.7.7"
+RELEASE_FILE="/etc/os-release"
+TIME_STAMP=$(date +"%d/%m/%Y %H:%M:%S")
 # Define colour codes
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Colour
 
-
-VERSION="2.7.6"
-RELEASE_FILE="/etc/os-release"
-TIME_STAMP=$(date +"%d/%m/%Y %H:%M:%S")
 
 function exit_code() {
     if [ $? -eq 0 ]
@@ -25,16 +24,18 @@ echo -e "${YELLOW}running Version $VERSION $TIME_STAMP ${NC}"
 if grep -q "Arch" $RELEASE_FILE
 then
     # The host is based on Arch, run the pacman update command
+    echo -e "${YELLOW}The host is based on Arch $TIME_STAMP ${NC}"          
     sudo pacman -Syu
     exit_code                                                               # Checks if the update was successful
 elif
-    echo -e "${YELLOW}Host is not based on Arch $TIME_STAMP ${NC}"
+    echo -e "${YELLOW}The host is not based on Arch $TIME_STAMP ${NC}"
     grep -q "Debian" $RELEASE_FILE || grep -q "Ubuntu" $RELEASE_FILE || grep -q "Pop" $RELEASE_FILE # "||"=or
 then
     # The host is based on Debian, Ubuntu or Pop
     # Run the apt version of the command
+    echo -e "${YELLOW}The host is based on Debian $TIME_STAMP ${NC}"
     sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
     exit_code                                                               # Checks if the update was successful
 else
-    echo -e "${YELLOW}Host is not based on Debian $TIME_STAMP ${NC}"
+    echo -e "${YELLOW}The host is not based on Debian $TIME_STAMP ${NC}"
 fi
