40ae90220712a7de629fcba4bc4e8353678070ab
Kotaro117 <hobble-choir-0m@icloud.com>
2024-02-29 09:57:33 +0000

    Update update_system.sh
    added colours to echo output for easier troubleshooting

--- update/update_system.sh
+++ update/update_system.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-VERSION="2.6"
+# Define color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+
+VERSION="2.7"
 RELEASE_FILE="/etc/os-release"
 
 function exit_code() {
     if [ $? -eq 0 ]
     then
-        echo "Update was successful"
+        echo -e "${GREEN}Update was successful ${NC}"
     else
-        echo "Update was not successful"
+        echo "${RED}Update was not successful ${NC}"
     fi
 }
 
-echo "running Version $VERSION"
+echo "${YELLOW}running Version $VERSION ${NC}"
 
 if grep -q "Arch" $RELEASE_FILE
 then
@@ -20,7 +27,7 @@ then
     sudo pacman -Syu
     exit_code
 else
-    echo "Host is not based on Arch"
+    echo "${YELLOW}Host is not based on Arch ${NC}"
 fi
 
 if grep -q "Debian" $RELEASE_FILE || grep -q "Ubuntu" $RELEASE_FILE || grep -q "Pop" $RELEASE_FILE # "||"=or
@@ -30,5 +37,9 @@ then
     sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
     exit_code
 else
-    echo "Host is not based on Debian"
+    echo "${YELLOW}Host is not based on Debian ${NC}"
 fi
+
+
+
+echo -e "$GREEN Update was successful $NC"
