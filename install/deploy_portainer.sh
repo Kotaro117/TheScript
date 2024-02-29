ed845113d4bfa43762b86f403aea6e7d9d5611e9
Kotaro117 <hobble-choir-0m@icloud.com>
2024-02-29 12:40:54 +0000

    Update deploy_portainer.sh
    Added a few lines for easier troubleshooting

--- install/deploy_portainer.sh
+++ install/deploy_portainer.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
-# V 1.0
+VERSION="2"
+TIME_STAMP=$(date +"%d/%m/%Y %H:%M:%S")
+# Define colour codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Colour
+
+
+function exit_code() {
+    if [ $? -eq 0 ]
+    then
+        echo -e "${GREEN}$COMMAND was successful $TIME_STAMP ${NC}"
+    else
+        echo -e "${RED}$COMMAND was not successful $TIME_STAMP ${NC}"
+    fi
+}
 
 # Create the volume that Portainer Server will use to store its database
+COMMAND="Creation of the Portainer volume"
 sudo docker volume create portainer_data
+exit_code
 
 # Download and install the Portainer Server container
+COMMAND="Creation and installation of the Portainer Server container"
 sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+exit_code
