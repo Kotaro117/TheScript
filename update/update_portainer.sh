e4484eac7ca0dff964fa978fe6a4e9fa2ec44556
Kotaro117 <hobble-choir-0m@icloud.com>
2024-02-29 12:30:42 +0000

    Update update_portainer.sh

--- update/update_portainer.sh
+++ update/update_portainer.sh
@@ -36,5 +36,7 @@ COMMAND="Download the latest image of Portainer"
 sudo docker pull portainer/portainer-ce:latest
 exit_code
 
+# Deploy updated version of Portainer
+COMMAND="Deploy updated version of Portainer"
 sudo docker run -d -p 8000:8000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 exit_code
