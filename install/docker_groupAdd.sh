f84c703d530771a4a2cb585244bca5aa2653a4df
Kotaro117 <hobble-choir-0m@icloud.com>
2024-02-29 13:16:19 +0000

    Update docker_groupAdd.sh
    replaced whiptail with echo

--- install/docker_groupAdd.sh
+++ install/docker_groupAdd.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 
-VERSION="V 2.2"
-TITLE="Docker permission $VERSION"
+VERSION="3"
+TIME_STAMP=$(date +"%d/%m/%Y %H:%M:%S")
+# Define colour codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Colour
 
-# Check for dependencies (Whiptail)
-command -v whiptail >/dev/null 2>&1 || { 
-    echo >&2 "whiptail is required but not installed. Would you like to install it?";
-    read -p "Install whiptail? (y/n): " answer
-        if [ "$answer" == "y" ]; then
-        sudo apt-get update
-        sudo apt-get install -y whiptail
+function exit_code() {
+    if [ $? -eq 0 ]
+    then
+        echo -e "${GREEN}User has been added to the Docker group. Please log out and log back in for the changes to take effect. $TIME_STAMP ${NC}"
     else
-        echo "Aborting."
-        exit 1
+        echo -e "${RED}User has NOT been added to the Docker group. $TIME_STAMP ${NC}"
     fi
 }
 
+echo -e "${YELLOW}running Version $VERSION of the script $TIME_STAMP ${NC}"
+
 if [ "$(groups | grep -c docker)" -eq 1 ]
 then
-    whiptail --title "$TITLE" --msgbox "Your user <$USER> is allready a member of the docker group" 8 60
+    echo -e "${YELLOW}Your user <$USER> is allready a member of the docker group $TIME_STAMP ${NC}" 
 else
-    whiptail --title "$TITLE" --yesno "Do you want to add your user <$USER> to the Docker group?" 8 60
-            if [ $? -eq 0 ]; then
-                sudo usermod -aG docker $USER # Add current user to the docker group
-                whiptail --title "$TITLE" --msgbox "User has been added to the Docker group. Please log out and log back in for the changes to take effect." 8 60
-            else
-                whiptail --title "$TITLE" --msgbox "User has not been added to the Docker group" 8 60
-            fi
+    sudo usermod -aG docker $USER # Add current user to the docker group
+    exit_code
 fi
