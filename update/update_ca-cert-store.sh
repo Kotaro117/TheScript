6db7e316b309ce427fdb1c74ce1b0e9e74e636ff
Kotaro117 <hobble-choir-0m@icloud.com>
2024-03-06 12:57:27 +0100

    Create update_ca-cert-store.sh

---
+++ update/update_ca-cert-store.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+VERSION="0.1"
+TIME_STAMP=$(date +"%d/%m/%Y %H:%M:%S")
+# Define colour codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Colour
+
+function exit_code() {
+    if [ $? -eq 0 ]
+    then
+        echo -e "${GREEN}$COMMAND was successfully $TIME_STAMP ${NC}"
+    else
+        echo -e "${RED}$COMMAND was not successful $TIME_STAMP ${NC}"
+    fi
+}
+
+echo "Where is your CA certificate file stored? eg. /folder/file.crt"
+    read CA_FILE
+COMMAND="Copie of the CA certificate"
+sudo cp $CA_FILE /usr/local/share/ca-certificates/
+exit_code
+COMMAND="Update of the CA certificate store"
+sudo update-ca-certificates
+exit_code
