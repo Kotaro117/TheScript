1460e04142b714a830c98d364d6561c20b5c7c88
Kotaro117 <hobble-choir-0m@icloud.com>
2024-04-12 16:29:16 +0200

    Update install_yaru.sh
    implemented a check for "ubuntu-session"

--- install/install_yaru.sh
+++ install/install_yaru.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="0.2.1"
+VERSION="0.3.1"
 TIME_STAMP=$(date +"%d/%m/%Y %H:%M:%S")
 # Define colour codes
 RED='\033[0;31m'
@@ -18,25 +18,28 @@ mkdir -p logs # create log folder if not present
 echo "" >> $log # add a new line to make it easier to read
 echo -e "${YELLOW}running Version $VERSION of the script $TIME_STAMP ${NC}"
 echo "$TIME_STAMP running Version $VERSION of the script" >> $log
+sudo apt update
 
 # Install gnome icons etc.
 sudo apt install -y yaru-theme-gnome-shell yaru-theme-gtk yaru-theme-icon yaru-theme-sound gnome-tweaks
 
-# Install ubuntu session
-read -p "Do you want to install Ubuntu session too? (y/n): " answer
-if [ "$answer" == "y" ]
-then
-    echo "User has chosen to install ubuntu-session" >> $log
-    sudo apt install -y ubuntu-session
-    echo -e "${YELLOW}In order to use ubuntu session log out, click on the cog and choose ubuntu ${NC}"
-    read -p "Do you want to log out? (y/n): " log_out
-    if [ "$log_out" == "y" ]
+# Checks whether ubuntu session is installed and aks to installed it if not.
+command -v ubuntu-session >/dev/null 2>&1 || {
+    read -p "Do you want to install Ubuntu session too? (y/n): " answer
+    if [ "$answer" == "y" ]
     then
-        echo "User has chosen to log out" >> $log
-        gnome-session-quit --logout --no-prompt
+        echo "User has chosen to install ubuntu-session" >> $log
+        sudo apt install -y ubuntu-session
+        echo -e "${YELLOW}In order to use ubuntu session log out, click on the cog and choose ubuntu ${NC}"
+        read -p "Do you want to log out? (y/n): " log_out
+        if [ "$log_out" == "y" ]
+        then
+            echo "User has chosen to log out" >> $log
+            gnome-session-quit --logout --no-prompt
+        else
+            echo "User has chosen not to log out" >> $log
+        fi
     else
-        echo "User has chosen not to log out" >> $log
+        echo "User has chosen not to install ubuntu-session" >> $log
     fi
-else
-    echo "User has chosen not to install ubuntu-session" >> $log
-fi
+}
