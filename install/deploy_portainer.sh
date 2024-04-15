e8106eeca46eedf1e12fd83df403c50ced220914
Kotaro117 <hobble-choir-0m@icloud.com>
2024-04-15 19:08:12 +0200

    Update deploy_portainer.sh
    Separated the script into sections

--- install/deploy_portainer.sh
+++ install/deploy_portainer.sh
@@ -1,27 +1,48 @@
 #!/bin/bash
 
-VERSION="2.3"
+
+########################
+### Variable section ###
+########################
+
+VERSION="2.4"
 TIME_STAMP=$(date +"%d/%m/%Y %H:%M:%S")
 # Define colour codes
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Colour
-log=deploy_portainer.txt
+log=logs/deploy_portainer.txt
 
-mkdir -p logs # create log folder if not present
-echo "" >> logs/$log # add a new line to make it easier to read
+
+########################
+### Function section ###
+########################
 
 function exit_code() {
     if [ $? -eq 0 ]
     then
-        echo -e "${GREEN}$COMMAND was successfully $TIME_STAMP ${NC}"; echo "$TIME_STAMP $COMMAND was successful" >> logs/$log
+        echo -e "${GREEN}$COMMAND was successfully $TIME_STAMP ${NC}"
+        echo "$TIME_STAMP $COMMAND was successful" >> $log
     else
-        echo -e "${RED}$COMMAND was not successful $TIME_STAMP ${NC}"; echo "$TIME_STAMP ERROR $COMMAND was not successful" >> logs/$log
+        echo -e "${RED}$COMMAND was not successful $TIME_STAMP ${NC}"
+        echo "$TIME_STAMP ERROR $COMMAND was not successful" >> $log
     fi
 }
 
-echo -e "${YELLOW}running Version $VERSION of the script $TIME_STAMP ${NC}"; echo "$TIME_STAMP running Version $VERSION of the script" >> logs/$log
+
+###############################
+### Beginning of the script ###
+###############################
+
+mkdir -p logs # create log folder if not present
+echo "" >> $log # add a new line to make it easier to read
+
+echo -e "${YELLOW}running Version $VERSION of the script $TIME_STAMP ${NC}"
+echo "$TIME_STAMP running Version $VERSION of the script" >> $log
+
+echo "Scripts is executed by $USER" >> $log
+groups | grep -q '\bsudo\b' && echo "User has sudo permissions" >> $log || echo "User does not have sudo permissions" >> $log
 
 # Create the volume that Portainer Server will use to store its database
 COMMAND="Creation of the Portainer volume"
@@ -30,7 +51,8 @@ then
     sudo docker volume create portainer_data
     exit_code
 else
-    echo -e "${YELLOW}Volume has not been created because it's allready there $TIME_STAMP ${NC}"; echo "$TIME_STAMP Volume has not been created because it's allready there" >> logs/$log
+    echo -e "${YELLOW}Volume has not been created because it's allready there $TIME_STAMP ${NC}"
+    echo "Volume has not been created because it's allready there" >> $log
 fi
 
 # Download and install the Portainer Server container
